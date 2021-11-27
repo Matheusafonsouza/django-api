1c104d43a842a5b0462b1a1ea5c0f633d023563d
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-27 02:00:25 -0300

    feat: school app, models, enum and settings

---
+++ school/enums.py
@@ -0,0 +1,10 @@
+from common.enums import BaseEnum
+
+
+class CourseLevelEnum(BaseEnum):
+    """
+    Enum for course dificulty levels.
+    """
+    EASY = 'EASY'
+    MEDIUM = 'MEDIUM'
+    HARD = 'HARD'
---
+++ school/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from school.enums import CourseLevelEnum
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    rg = models.CharField(max_length=9)
+    cpf = models.CharField(max_length=1)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.CharField(max_length=100)
+    level = models.CharField(
+        max_length=10,
+        choices=CourseLevelEnum.choices(),
+        blank=False,
+        null=False,
+        default=CourseLevelEnum.EASY
+    )
+
+    def __str__(self):
+        return self.description
