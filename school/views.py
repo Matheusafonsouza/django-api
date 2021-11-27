334c5c45d4243dec5c5f3eb569dbcbb0faf691b8
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-27 02:10:57 -0300

    feat: course and student viewset

--- school/views.py
+++ school/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from school.models import Student, Course
+from school.serializers import StudentSerializer, CourseSerializer
+
+
+class StudentsViewSet(ModelViewSet):
+    """"
+    View set for HTTP requests over Student model
+    """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class CoursesViewSet(ModelViewSet):
+    """"
+    View set for HTTP requests over Course model
+    """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
