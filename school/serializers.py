3bf4493f8dfe8cd9db730195d08902d9944fda52
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-27 02:12:01 -0300

    docs: serializers docstring

--- school/serializers.py
+++ school/serializers.py
@@ -3,12 +3,18 @@ from school.models import Student, Course
 
 
 class StudentSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for do and undo serialization over the student model.
+    """
     class Meta:
         model = Student
         fields = '__all__'
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for do and undo serialization over the course model.
+    """
     class Meta:
         model = Course
         fields = '__all__'
