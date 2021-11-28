6d6a1bb93d216395df1450569682ec82a9b66eae
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-28 14:35:05 -0300

    feat: ordering on client view set

--- client/views.py
+++ client/views.py
@@ -4,6 +4,8 @@ from client.models import Client
 from client.serializers import ClientSerializer
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ClientsViewSet(ModelViewSet):
@@ -14,3 +16,5 @@ class ClientsViewSet(ModelViewSet):
     serializer_class = ClientSerializer
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering_fields = ['name']
