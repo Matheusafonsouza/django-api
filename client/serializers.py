3d77533536347b6136671613ae00ee85c4893a6c
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-28 13:44:51 -0300

    chore: all validations for client on client serializer

--- client/serializers.py
+++ client/serializers.py
@@ -17,6 +17,38 @@ class ClientSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('CPF field must have 11 digits.')
         return cpf
 
+    def validate_rg(self, rg):
+        """
+        Method for validate rg conditions.
+        :param rg: Client instance rg
+        :returnts: The rg if validation is ok or raises a validation error
+        """
+        if len(rg) != 9:
+            raise serializers.ValidationError('RG field must have 9 digits.')
+        return rg
+
+    def validate_phone(self, phone):
+        """
+        Method for validate phone conditions.
+        :param phone: Client instance phone
+        :returnts: The phone if validation is ok or raises a validation error
+        """
+        if len(phone) != 11:
+            raise serializers.ValidationError(
+                'Phone field must have 11 digits.')
+        return phone
+
+    def validate_name(self, name):
+        """
+        Method for validate name conditions.
+        :param name: Client instance name
+        :returnts: The name if validation is ok or raises a validation error
+        """
+        if not name.isalpha():
+            raise serializers.ValidationError(
+                'Name field must have only alpha characters.')
+        return name
+
     class Meta:
         model = Client
         fields = '__all__'
