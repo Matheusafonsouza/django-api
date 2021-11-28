ea5941b035cc989624bde6f2f1826752080d001e
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-28 13:41:18 -0300

    feat: cpf validation on client serializer

--- client/serializers.py
+++ client/serializers.py
@@ -6,6 +6,17 @@ class ClientSerializer(serializers.ModelSerializer):
     """
     Serializer class for do and undo serialization over the client model.
     """
+
+    def validate_cpf(self, cpf):
+        """
+        Method for validate cpf conditions.
+        :param cpf: Client instance cpf
+        :returnts: The cpf if validation is ok or raises a validation error
+        """
+        if len(cpf) != 11:
+            raise serializers.ValidationError('CPF field must have 11 digits.')
+        return cpf
+
     class Meta:
         model = Client
         fields = '__all__'
