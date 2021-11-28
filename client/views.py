85e1e582b5ea2d8cb6dac138247af6e70a35ada6
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-28 14:39:10 -0300

    feat: search query filters

--- client/views.py
+++ client/views.py
@@ -4,7 +4,7 @@ from client.models import Client
 from client.serializers import ClientSerializer
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.filters import OrderingFilter
+from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -16,5 +16,7 @@ class ClientsViewSet(ModelViewSet):
     serializer_class = ClientSerializer
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
-    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
     ordering_fields = ['name']
+    search_fields = ['name', 'cpf']
+    filterset_fields = ['active']
